caac6540b49fd453f574dc4149efcda1d585f11d
Cecilia Sjögren <cisci_13@hotmail.com>
2021-12-03 22:33:39 +0100

    Merged old repos

---
+++ Exercise_1/Exercise1.2/Exercise1.2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Exercise1._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteMenuText();
+        }
+        static private void WriteMenuText() //Presents the menu choices to the user 
+        {
+            Console.WriteLine(""); //Emty Line to create space
+            Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+            Console.WriteLine("{0, 25}", "MAIN MENU");
+            Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+            Console.WriteLine("Add new employee: {0, 7}", '1');
+            Console.WriteLine("Show employee List: {0, 5}", '2');
+            Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+            Console.Write("Make your choice: ");
+        }
+    }
+}
