15b4b5d2f05ca80f8fd408572bc653038ac39608
Cecilia Sjögren <cisci_13@hotmail.com>
2021-12-07 15:44:08 +0100

    Created the project and started some code

---
+++ Exercise_3/Exercise_3/Person.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_3
+{
+    internal class Person
+    {
+        private int age;
+        private string fName;
+        private string lName;
+        private double height;
+        private double weight;
+
+        public int Age
+        {
+            get { return myVar; }
+            set { myVar = value; }
+        }
+
+        public string FName { get; set; }
+        public string LName { get; set; }
+        public double Height { get; set; }
+        public double Weight { get; set; }
+        //F: Nej jag kommer åt propertyn för variablen men inte variablen direkt.
+    }
+}
---
+++ Exercise_3/Exercise_3/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Exercise_3;
+
+var Lasse = new Person();
+Lasse.Age = 30;
+
+
