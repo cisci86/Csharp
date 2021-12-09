19de645aa0389540fca51d3d04269ef05db1008f
Cecilia Sjögren <cisci_13@hotmail.com>
2021-12-09 10:20:23 +0100

    Entered some dummy data in Program to test the Person and PersonHandler code and also testing the exception casting.

--- Exercise_3/Exercise_3/Program.cs
+++ Exercise_3/Exercise_3/Program.cs
@@ -5,13 +5,37 @@ using Exercise_3;
 //Lasse.Age = 30;
 
 PersonHandler pHandler = new PersonHandler();
+//To catch the exception that might be cast in the Person class.
 try
 {
-    pHandler.CreatePerson(23, "Lisa", "S", 163, 75);
+    //Just trying out the code by creating a fake person. Testing the other methods in the PersonHandler.
+ var lisa = pHandler.CreatePerson(23, "Lisa", "Svensson", 163, 75);
+    Console.WriteLine(pHandler.GetFullName(lisa));
+    Console.WriteLine(pHandler.GetAge(lisa));
+    
+}
+catch (ArgumentException ex)
+{
+    Console.WriteLine(ex.Message);
+}
+try
+{
+  var adam = pHandler.CreatePerson(30, "Adam", "Avensson", 180, 83);
+    Console.WriteLine($"Name: {adam.FName} {adam.LName}, age: {adam.Age}years");
 }
 catch (ArgumentException ex)
 {
     Console.WriteLine(ex.Message);
 }
 
+try
+    //Testing the Try/Catch part.
+{
+    var lotta = pHandler.CreatePerson(38, "Lisa", "P", 171, 74);
+    Console.WriteLine($"Name: {lotta.FName} {lotta.LName}, age: {lotta.Age}years");
+}
+catch (ArgumentException ex)
+{
+    Console.WriteLine(ex.Message);
+}
 
